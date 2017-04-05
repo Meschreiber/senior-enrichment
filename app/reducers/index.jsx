c6ed31a3444745103361dcc09ca2b0ca66f77674
Maria Schreiber <meschreiber3@gmail.com>
2017-04-05 18:31:23 -0400

    Created reducers

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -1,9 +1,19 @@
-import { combineReducers } from 'redux'
+// import { combineReducers } from 'redux'
+import { RECEIVE_CAMPUSES, RECEIVE_STUDENTS } from '../action-creators/index'
 
-const initialState = {}
+const initialState = { campuses: [], students: [] }
 
-const rootReducer = function(state = initialState, action) {
-  switch(action.type) {
+const rootReducer = function (state = initialState, action) {
+  const newState = Object.assign({}, state);
+  switch (action.type) {
+    case RECEIVE_CAMPUSES:
+      newState.campuses = action.campuses;
+      break;
+
+    case RECEIVE_STUDENTS:
+      newState.students = action.students;
+      break;
+
     default: return state
   }
 };
